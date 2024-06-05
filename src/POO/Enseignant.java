b1a72c496b0eb0481d9d799a5276ec45c439fa5e
noureddinestam <noureddinestam@gmail.com>
2024-06-05 12:52:49 +0100

    Exercice 4

---
+++ src/POO/Enseignant.java
@@ -0,0 +1,36 @@
+package POO;
+
+public class Enseignant extends Individu {
+	private String specialisation;
+	private Double salaireMensuel;
+	
+	public Enseignant(String nom, String prenom, int identifiant, String specialisation, Double salaireMensuel) {
+		super(nom, prenom, identifiant);
+		this.specialisation = specialisation;
+		this.salaireMensuel = salaireMensuel;
+		
+	}
+
+	public String getSpecialisation() {
+		return specialisation;
+	}
+
+	public void setSpecialisation(String specialisation) {
+		this.specialisation = specialisation;
+	}
+
+	public Double getSalaireMensuel() {
+		return salaireMensuel;
+	}
+
+	public void setSalaireMensuel(Double salaireMensuel) {
+		this.salaireMensuel = salaireMensuel;
+	}
+
+	@Override
+	public void montrerDetails() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
