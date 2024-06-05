ebebd698eccc8bb9e99e5d70f58da60881b4b0c1
noureddinestam <noureddinestam@gmail.com>
2024-06-05 12:10:06 +0100

    Exercice 1

--- src/POO/Etudiant.java
+++ src/POO/Etudiant.java
@@ -1,5 +1,36 @@
 package POO;
 
-public class Etudiant {
+class Etudiant extends Individu {
+    private Date dateInscription;
+    private String niveauEtude;
 
+    public Etudiant(String nom, String prenom, int identifiant, Date dateInscription, String niveauEtude) {
+        super(nom, prenom, identifiant);
+        this.dateInscription = dateInscription;
+        this.niveauEtude = niveauEtude;
+    }
+
+
+    public Date getDateInscription() {
+        return dateInscription;
+    }
+
+    public void setDateInscription(Date dateInscription) {
+        this.dateInscription = dateInscription;
+    }
+
+    public String getNiveauEtude() {
+        return niveauEtude;
+    }
+
+    public void setNiveauEtude(String niveauEtude) {
+        this.niveauEtude = niveauEtude;
+    }
+
+    @Override
+    public void montrerDetails() {
+        System.out.println("Nom: " + getNom() + ", Prénom: " + getPrenom() +
+                ", Identifiant: " + getIdentifiant() + ", Date d'inscription: " +
+                dateInscription + ", Niveau d'étude: " + niveauEtude);
+    }
 }
